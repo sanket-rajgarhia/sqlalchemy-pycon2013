1742902f199ab471b0e0e191fd3e89177f4e7726
Sanket Rajgarhia <sanket.rajgarhia@gmail.com>
2018-07-26 20:20:11 +0530

    Created the py-code/setup.sql file for setting up engine.db

---
+++ py-code/setup.sql
@@ -0,0 +1,13 @@
+create table employee(
+        emp_id integer primary key,
+        emp_name varchar
+    );
+
+create table employee_of_month(
+        emp_id integer primary key,
+        emp_name varchar
+    );
+
+insert into employee(emp_name) values ('ed');
+insert into employee(emp_name) values ('jack');
+insert into employee(emp_name) values ('fred');
