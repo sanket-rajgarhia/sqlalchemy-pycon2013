af6244ac48f4ca97a0028e4422a4e41c43f85f15
Sanket Rajgarhia <sanket.rajgarhia@gmail.com>
2018-07-26 22:36:38 +0530

    Demonstrating - fetchall() returns a list of rows.

--- py-code/01-engine.py
+++ py-code/01-engine.py
@@ -13,15 +13,17 @@ def print_output(number,code,heading):
     return
 ################################################################################
 
-#1.
+#1
 #engine is a factory for database connections.
-engine = create_engine("sqlite:///engine.db")
 number = 1
 code = """
 engine = create_engine("sqlite:///engine.db")
 """
 heading = "Initializing the engine."
 print_output(number,code,heading)
+
+#Code
+engine = create_engine("sqlite:///engine.db")
 
 input("\nEnter to continue...")
 
@@ -30,19 +32,20 @@ input("\nEnter to continue...")
 #2
 #Demonstrating the execute() method to get the result of the query.
 #and also using fetchone() on the result.
-result = engine.execute("select emp_id, emp_name from employee "
-                        "where emp_id=:empid", empid = 3)
-row = result.fetchone()
-result.close()
-
-number = 1
+number += 1
 code = """
 result = engine.execute("select emp_id, emp_name from employee "
                         "where emp_id=:empid", empid = 3)
 row = result.fetchone()
 """
-heading = "Accessing 'row'"
+heading = "Accessing 'row'."
 print_output(number,code,heading)
+
+#Code
+result = engine.execute("select emp_id, emp_name from employee "
+                        "where emp_id=:empid", empid = 3)
+row = result.fetchone()
+result.close()
 print("row : {}".format(row))
 print("row['emp_name'] : {}".format(row['emp_name']))
 print("row.emp_name : {}".format(row.emp_name))
@@ -53,18 +56,36 @@ input("\nEnter to continue...")
 
 #3
 #Demonstrating that result is iterable.
-result = engine.execute("select * from employee")
-
+number += 1
 code = """
 result = engine.execute("select * from employee")
 for row in result:
     print(row)
 """
-heading = "'result' is iterable"
-number += 1
+heading = "The object 'result' is iterable."
 print_output(number, code, heading)
+
+#Code
+result = engine.execute("select * from employee")
 for row in result:
     print(row)
+
+input("\nEnter to continue...")
+
+################################################################################
+
+#4
+#Demonstrating fetchall() to fetch a list
+number += 1
+code = """
+result = engine.execute("select * from employee")
+print(result.fetchall())
+"""
+heading = "Demonstrating - fetchall() returns a list of rows"
+print_output(number,code,heading)
+
+result = engine.execute("select * from employee")
+print(result.fetchall())
 
 input("\nEnter to continue...")
 
