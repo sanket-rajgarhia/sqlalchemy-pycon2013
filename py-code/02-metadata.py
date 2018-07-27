0dc2193cd2b153bc84c8e367420672bd96eb1814
Sanket Rajgarhia <sanket.rajgarhia@gmail.com>
2018-07-27 16:01:29 +0530

    Created the MetaData object and added the 'user' Table to it.

--- py-code/02-metadata.py
+++ py-code/02-metadata.py
@@ -1,6 +1,10 @@
 import os
 from termcolor import colored, cprint
 
+from sqlalchemy import MetaData
+from sqlalchemy import Table, Column
+from sqlalchemy import Integer, String
+
 #Restore the state of metadata.db - prior to run
 os.system('git checkout -- metadata.db')
 
@@ -18,11 +22,37 @@ def print_output(number,code,heading):
 ################################################################################
 
 #1
+#Using the MetaData() object to containg the structure of the schema.
+#Create the 'user' table.
 number = 1
 code = """
+metadata = MetaData()
+user_table = Table('user', metadata,
+    Column('id', Integer, primary_key = True),
+    Column('name', String),
+    Column('fullname', String))
 """
-heading = ""
+heading = "Listing the attributes of user_table."
 print_output(number,code,heading)
+
+metadata = MetaData()
+user_table = Table('user', metadata,
+    Column('id', Integer, primary_key = True),
+    Column('name', String),
+    Column('fullname', String))
+
+#List attributes of user_table
+print("user_table.name : {}". format(user_table.name))
+print("user_table.columns : {}".format(user_table.columns))
+print("user_table.columns.id : {}".format(user_table.columns.id))
+print("user_table.columns.id.type : {}".format(user_table.columns.id.type))
+print("user_table.columns.name.name : {}".format(user_table.columns.name.name))
+print("user_table.columns.keys() : {}".format(user_table.columns.keys()))
+name_column = user_table.columns.name
+print("name_column : {}".format(name_column))
+print("name_column.table : {}".format(name_column.table))
+print("user_table.primary_key : {}".format(user_table.primary_key))
+print("user_table.select() : {}".format(user_table.select()))
 
 input("\nEnter to continue...")
 ################################################################################
