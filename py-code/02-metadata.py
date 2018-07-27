b9a2990ff84b5ccc72cd5aebe237c0774d1891a9
Sanket Rajgarhia <sanket.rajgarhia@gmail.com>
2018-07-27 15:24:27 +0530

    Initialized the 02-metadata.py file.

---
+++ py-code/02-metadata.py
@@ -0,0 +1,36 @@
+import os
+from termcolor import colored, cprint
+
+#Restore the state of metadata.db - prior to run
+os.system('git checkout -- metadata.db')
+
+#Function to print heading and code
+def print_output(number,code,heading):
+    os.system('clear')
+    number_print = "\nNo {}.".format(number)
+    print(number_print)
+    print("_"*len(number_print))
+    print("{}".format(code).rstrip('\n'))
+    print("\n{}".format(heading))
+    print("-"*len(heading.split('\n')[0]))
+    return
+
+################################################################################
+
+#1
+number = 1
+code = """
+"""
+heading = ""
+print_output(number,code,heading)
+
+input("\nEnter to continue...")
+################################################################################
+
+os.system('clear')
+print("\n"* 5)
+cprint("END".rjust(38, " "), 'blue', attrs=['bold'])
+heading = "NOTE: RESET YOUR GIT : git checkout -- metadata.db"
+heading = heading.rjust(( len(heading) + ((80 - len(heading)) // 2)), " ")
+cprint('{}'.format(heading), 'red', attrs=['blink','bold'])
+print("\n"* 5)
