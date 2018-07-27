664fb340f493cb3be33719b69f37cbe513f440b8
Sanket Rajgarhia <sanket.rajgarhia@gmail.com>
2018-07-27 15:27:02 +0530

    Created a template 'template.py' for python code files.

---
+++ py-code/template.py
@@ -0,0 +1,38 @@
+import os
+from termcolor import colored, cprint
+
+#Replace <Database_file> and remove this comment.
+
+#Restore the state of metadata.db - prior to run
+os.system('git checkout -- <Database_file>')
+
+#Function to print heading and code
+def print_output(number,code,heading):
+    os.system('clear')
+    number_print = "\nNo {}.".format(number)
+    print(number_print)
+    print("_"*len(number_print))
+    print("{}".format(code).rstrip('\n'))
+    print("\n{}".format(heading))
+    print("-"*len(heading.split('\n')[0]))
+    return
+
+################################################################################
+
+#1
+number = 1
+code = """
+"""
+heading = ""
+print_output(number,code,heading)
+
+input("\nEnter to continue...")
+################################################################################
+
+os.system('clear')
+print("\n"* 5)
+cprint("END".rjust(38, " "), 'blue', attrs=['bold'])
+heading = "NOTE: RESET YOUR GIT : git checkout -- <Database_file>"
+heading = heading.rjust(( len(heading) + ((80 - len(heading)) // 2)), " ")
+cprint('{}'.format(heading), 'red', attrs=['blink','bold'])
+print("\n"* 5)
