23f5b45e231fdc31dc8d9e00c1b496f9f96efe36
Sanket Rajgarhia <sanket.rajgarhia@gmail.com>
2018-08-04 18:45:59 +0530

    Updated the 'code' variable for #1.

--- py-code/05-orm-query.py
+++ py-code/05-orm-query.py
@@ -57,8 +57,28 @@ session.commit()
 #Using the Domain Model object oriented classes.
 number = 1
 code = """
-print("Attributes of Domain Model classes act as Column() objects.")
+print("Attributes of Domain Model classes act just as Column() objects.")
 print(User.name == 'ed')
+print("-" * 80)
+print("To get the Column() object - use property.columns")
+print("User.name.property.columns[0]: {}".format(User.name.property.columns[0]))
+print("-" * 80)
+print("Using the Domain Model object instead of table and column objects in \
+SQL Expression.")
+select_stmt = select([User.id.label("ID"), User.name, User.fullname]).where(
+                      User.name.in_(['ed','wendy'])).order_by(User.name.desc())
+print(select_stmt)
+print("-" * 80)
+print("Use session.connection().execute(query) to execute a query on a \
+database that the engine is connected to.")
+result = session.connection().execute(select_stmt).fetchall()
+print(result)
+print("-" * 80)
+print("Using ORM session.query() with Domain Model OO class.")
+query = session.query(User.id.label("ID"), User.name, User.fullname).filter(
+                       User.name.in_(['ed','wendy'])).order_by(User.name.desc())
+result = query.all()
+print(result)
 """
 heading = "Using the Domain Model object oriented classes."
 print_output(number,code,heading)
@@ -85,6 +105,7 @@ query = session.query(User.id.label("ID"), User.name, User.fullname).filter(
                        User.name.in_(['ed','wendy'])).order_by(User.name.desc())
 result = query.all()
 print(result)
+
 input("\nEnter to continue...")
 
 ################################################################################
